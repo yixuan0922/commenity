2eeb7cf8ef525d3280ca63b1b3316fce4861eef9
yixuan0922 <yixuan0922@hotmail.com>
2023-08-25 21:14:51 +0800

    Uploaded Template

---
+++ argon-pro-react-native-v1.9.1/App.js
@@ -0,0 +1,90 @@
+import React, { useCallback, useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { Block, GalioProvider } from "galio-framework";
+import { NavigationContainer } from "@react-navigation/native";
+import { Image } from "react-native";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// Before rendering any navigation stack
+import { enableScreens } from "react-native-screens";
+enableScreens();
+
+import Screens from "./navigation/Screens";
+import { Images, articles, argonTheme } from "./constants";
+
+// cache app images
+const assetImages = [
+  Images.Onboarding,
+  Images.LogoOnboarding,
+  Images.Logo,
+  Images.Pro,
+  Images.ArgonLogo,
+  Images.iOSLogo,
+  Images.androidLogo,
+];
+
+// cache product images
+articles.map((article) => assetImages.push(article.image));
+
+function cacheImages(images) {
+  return images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        //Load Resources
+        await _loadResourcesAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          "open-sans-regular": require("./assets/font/OpenSans-Regular.ttf"),
+          "open-sans-light": require("./assets/font/OpenSans-Light.ttf"),
+          "open-sans-bold": require("./assets/font/OpenSans-Bold.ttf"),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const _loadResourcesAsync = async () => {
+    return Promise.all([...cacheImages(assetImages)]);
+  };
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer onReady={onLayoutRootView}>
+      <GalioProvider theme={argonTheme}>
+        <Block flex>
+          <Screens />
+        </Block>
+      </GalioProvider>
+    </NavigationContainer>
+  );
+}
