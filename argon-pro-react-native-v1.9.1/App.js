eee062b780bc43c3e65a3e98f60f8000e9657aaf
yixuan0922 <yixuan0922@hotmail.com>
2023-08-27 16:19:57 +0800

    Fix: The fonts and images

--- argon-pro-react-native-v1.9.1/App.js
+++ argon-pro-react-native-v1.9.1/App.js
@@ -28,7 +28,7 @@ const assetImages = [
 ];
 
 // cache product images
-articles.map((article) => assetImages.push(article.image));
+// articles.map((article) => assetImages.push(article.image));
 
 function cacheImages(images) {
   return images.map((image) => {
